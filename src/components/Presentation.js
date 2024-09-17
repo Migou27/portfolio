2fd1e38e9b89460b27586be68c856f0f1e77c6ac
Migou27 <miguel.fenerol@gmail.com>
2024-09-17 11:24:18 +0200

    <master>(v2): update automne 2024

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -7,22 +7,23 @@ const Presentation = () => {
                   <div className='presBox'>
                         <p className='listeTitre'>A propos de moi : </p>
                         <p className='liste'>
-                              <br/>Je suis passionné de jeux vidéos et de développement. J'aime également le badminton, la cuisine et les mangas.<br/><br/>
+                              <br/>Je suis passionné de jeux vidéos et de développement. J'aime également le badminton, la cuisine et les mangas.
+                              <br/>Je suis également une personne curieuse, qui sait travaillé en équipe et de manière organisée.<br/>
                         </p>
                         <p className='listeTitre'>Mon projet professionnel :</p>
                         <div className='liste'>
-                              <br/>Devenir développeur de logiciel ou d'application web en allant jusqu'au bac+5. Pour cela je me suis inscrit au CESI de Rouen.<br/><br/>
+                              <br/>Devenir développeur de logiciel ou d'application web en allant jusqu'au bac+5. Pour cela je suis une formation bachelor puis master chez Rouen Ynov Campus.<br/><br/>
                         </div>
                         <p className='listeTitre'>Mon parcours :</p>
                         <div className='liste'>
                               <br/>
-                              <li>Brevet des collèges en 2015 au collège Léonard de Vinci de Saint Marcel (27)</li>
-                              <br/>
-                              <li>Baccalauréat scientifique spécialité Physique-Chimie en 2018 au <a href="https://malraux.lycee.ac-normandie.fr/">Lycée André Malraux</a> de Gaillon (27)</li>
+                              <li>BTS SIO (Services informatiques aux organisations) obtenu à l'<a href="https://www.stadjutor.com/">établissement Saint Adjutor</a> de Vernon (27)</li>
                               <br/>
                               <li>1ère année en Licence IEEEA à l’université de Rouen (76)</li>
                               <br/>
-                              <li>BTS SIO (Services informatiques aux organisations) en cours d’obtention à l'<a href="https://www.stadjutor.com/">établissement Saint Adjutor</a> de Vernon (27)</li>
+                              <li>Baccalauréat scientifique spécialité Physique-Chimie en 2018 au <a href="https://malraux.lycee.ac-normandie.fr/">Lycée André Malraux</a> de Gaillon (27)</li>
+                              <br/>
+                              <li>Brevet des collèges en 2015 au collège Léonard de Vinci de Saint Marcel (27)</li>
                         </div>
                   </div>
             </div>
